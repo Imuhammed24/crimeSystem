5e0057d2e0a24ddcc841f79d4bab9440664b8676
imuhammed24 <ishaqmuhammed011@gmail.com>
2020-12-22 19:21:18 +0100

    fixing delete record

--- crime_records/views.py
+++ crime_records/views.py
@@ -25,10 +25,11 @@ def crime_detail_view(request, record_id):
 def delete_crime_view(request, record_id):
     record = get_object_or_404(CrimeRecord, id=record_id)
     record.delete()
+    records = CrimeRecord.objects.all()
     context = {
-        'record': record,
-        'html_title': 'RECORD DEATIL VIEW',
-        'section': 'detail',
+        'records': records,
+        'html_title': 'EXPLORE RECORDS',
+        'section': 'explore',
     }
-    return render(request, 'crime_records/record/detail.html', context)
+    return render(request, 'crime_records/explore-all.html', context)
 
